92ed7d4ff061510a69eac16f8aa97701fc54594d
Jonatas Cavalcanti Pereira <jcp@intranet.pitang.com>
2017-06-22 16:06:11 -0300

    Agendamento do processamento em 3 em 3 minutos

--- tesouro-direto/teste-tesouro-direto.js
+++ tesouro-direto/teste-tesouro-direto.js
@@ -5,17 +5,15 @@ var updateLastDate = require('./update-last-date');
 var updateData = require('./update-data');
 var tesouroDiretoJson = require('./tesouro-direto-json');
 
-tesouroDiretoJson(function (json) {
-    
-    if (validarDataProcessamento(json)) {
+var j = schedule.scheduleJob('*/3 * * * *', function(){
+	tesouroDiretoJson(function (json) {
+	  if (validarDataProcessamento(json)) {
 		console.log('Nada a processar em: ' + new Date());
-	} else {
-		updateLastDate(json);
-		updateData(json);
-	}
-
-	/*var j = schedule.scheduleJob('15 * * * * *', function(){
-	  console.log('Teste do schedule!');
-	});*/
-
+		} else {
+			updateLastDate(json);
+			updateData(json);
+		}
+	});
 });
+
+
--- tesouro-direto/validar-data-processamento.js
+++ tesouro-direto/validar-data-processamento.js
@@ -5,6 +5,8 @@ var validarDataProcessamento = function(json) {
 	var lastDateProcessed = fs.readFileSync(__dirname + '/data.log');
 	var lastDateRequest = json.data;
 
+	console.log('Data último processamento: ' + lastDateProcessed + ' Data da requisição: ' + lastDateRequest);
+
 	return lastDateProcessed == lastDateRequest;
 };
 
