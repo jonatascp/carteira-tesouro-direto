df372b441e5d6f7741132734129650bad52f962c
Jonatas Cavalcanti Pereira <jcp@intranet.pitang.com>
2017-06-22 08:06:38 -0300

    Realizando o processamento dos dados e armazenando em um arquivo

--- tesouro-direto/teste-tesouro-direto.js
+++ tesouro-direto/teste-tesouro-direto.js
@@ -3,19 +3,35 @@ var schedule = require('node-schedule');
 var tesouroDiretoJson = require('./tesouro-direto-json');
 
 tesouroDiretoJson(function (json) {
-    var dadosTesouro = '\nData: ' + json.data;
-    dadosTesouro += '\n>>>>Títulos para Investir';
+    var dadosTesouro = 'Data: ' + json.data;
+    dadosTesouro += '\n>>>>Títulos para Investir<<<<<';
     for (index in json.titulosInvestir) {
 		dadosTesouro += '\n' + json.titulosInvestir[index].titulo + '\t' + json.titulosInvestir[index].vencimento + '\t' + json.titulosInvestir[index].taxaRendimento + '\t' + json.titulosInvestir[index].valorMinimo + '\t' + json.titulosInvestir[index].precoUnitario;
 	}
+	dadosTesouro += '\n>>>>Títulos para Resgatar<<<<<';
+	for (index in json.titulosResgatar) {
+		dadosTesouro += '\n' + json.titulosResgatar[index].titulo + '\t' + json.titulosResgatar[index].vencimento + '\t' + json.titulosResgatar[index].taxaRendimento + '\t' + json.titulosResgatar[index].precoUnitario + '\n';
+	}
     
-    fs.appendFile('data.log', dadosTesouro, function (err) {
-	   if (err) return console.log(err);
-	   console.log('Processamento realizando em: ' + new Date());
-	});
+    var lastDateProcessed = fs.readFileSync('data.log');
+	var lastDateRequest = json.data;
 
-	var j = schedule.scheduleJob('15 * * * * *', function(){
+	if (lastDateProcessed == lastDateRequest) {
+		console.log('Nada a processar em: ' + new Date());
+	} else {
+		fs.truncateSync('data.log');
+		fs.appendFile('data.log', lastDateRequest, function (err) {
+	   		if (err) return console.log(err);
+	   	});
+
+		fs.appendFile('dados.log', dadosTesouro, function (err) {
+	   		if (err) return console.log(err);
+	   		console.log('Processamento realizando em: ' + new Date());
+		});
+	}
+
+	/*var j = schedule.scheduleJob('15 * * * * *', function(){
 	  console.log('Teste do schedule!');
-	});
+	});*/
 
 });
